b455bf3e19b69f66344c814043e8e1b79cf77941
armie95 <115040104+armie95@users.noreply.github.com>
2022-11-21 21:38:56 -0500

    Created a simple route, configured the server, and env PORT variable, added the cors to express app, and added the static public folder to express middleware, and express.json as well. Created the app.listen function to run the server.

---
+++ index.js
@@ -0,0 +1,20 @@
+const dotenv = require("dotenv");
+const express = require("express");
+const cors = require("cors");
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 8080;
+
+console.clear();
+app.use(express.static("public"));
+app.use(cors());
+app.use(express.json());
+
+app.get("/", function (req, res) {
+  res.send("<h3 style='text-align: center;'>In Stock API - HOME PAGE</h3>");
+});
+
+app.listen(port, function () {
+  console.log(`Server is running on: http://localhost:${port} 🔥`);
+});
