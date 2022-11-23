fd91c38a5d3f93e0f4ecb0e8bcd736bdeabd150f
Jack Sprinkle <sprinkle.jas@gmail.com>
2022-11-23 14:31:32 -0500

    add index warehouse controller

--- controllers/warehouseController.js
+++ controllers/warehouseController.js
@@ -1,2 +1,12 @@
 //Import knex so the controller has access to the DB
 const knex = require('knex')(require('../knexfile'));
+
+exports.index = async (req, res) => {
+    try {
+        const warehouseData = await knex('warehouses')
+            .select('id', 'warehouse_name', 'address', 'city', 'country', 'contact_name', 'contact_phone', 'contact_email');
+            res.status(200).json(warehouseData);
+    } catch (err) {
+        res.status(400).send(`Error retrieving Warehouses: ${err}`)
+    }
+}
