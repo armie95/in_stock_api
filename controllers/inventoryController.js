3fde0d23f265e54cadb298bfde181b1b8615dadb
armie95 <115040104+armie95@users.noreply.github.com>
2022-11-24 02:27:56 -0500

    Created an API on the back-end to get all inventories for a warehouse by providing the warehouse ID

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -1,2 +1,27 @@
 //Import knex so the controller has access to the DB
-const knex = require('knex')(require('../knexfile'));
+const knex = require("knex")(require("../knexfile"));
+
+exports.getInventoriesByWarehouseId = async (req, res) => {
+  const warehouseId = req.params.id;
+
+  try {
+    const inventories = await knex("inventories")
+      .where({
+        warehouse_id: warehouseId,
+      })
+      .select(
+        "id",
+        "warehouse_id",
+        "item_name",
+        "description",
+        "category",
+        "status",
+        "quantity",
+        "created_at",
+        "updated_at"
+      );
+    res.status(200).json(inventories);
+  } catch (err) {
+    res.status(400).send(`Error retrieving Inventory Items: ${err}`);
+  }
+};
--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -1,4 +1,8 @@
-const router = require('express').Router();
-const inventoryController = require('../controllers/inventoryController');
+//Import router for necessary calls.
+//Import controller for all functions that need to modify the DB.
+const router = require("express").Router();
+const inventoryController = require("../controllers/inventoryController");
+
+router.route("/:id").get(inventoryController.getInventoriesByWarehouseId);
 
 module.exports = router;
