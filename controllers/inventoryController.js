dc2d060281aab467da1a6b9f3b2c4331dad96770
armie95 <115040104+armie95@users.noreply.github.com>
2022-11-24 02:05:19 -0500

    Created the route to get all inventory items in the database

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -1,2 +1,21 @@
 //Import knex so the controller has access to the DB
-const knex = require('knex')(require('../knexfile'));
+const knex = require("knex")(require("../knexfile"));
+
+exports.index = async (req, res) => {
+  try {
+    const inventoriesData = await knex("inventories").select(
+      "id",
+      "warehouse_id",
+      "item_name",
+      "description",
+      "category",
+      "status",
+      "quantity",
+      "created_at",
+      "updated_at"
+    );
+    res.status(200).json(inventoriesData);
+  } catch (err) {
+    res.status(400).send(`Error retrieving Warehouses: ${err}`);
+  }
+};
