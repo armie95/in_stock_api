c3b48f54a3252a74e5995cf709567a409ff53d5c
Sarai Escobar <saraiee17@yahoo.com>
2022-11-23 23:03:45 -0600

    firstcommit

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -1,2 +1,18 @@
 //Import knex so the controller has access to the DB
-const knex = require('knex')(require('../knexfile'));
+const knex = require('knex')(require('../knexfile')); 
+
+//API TO POST INVENTORY ITEM
+exports.addInventory = async (req, res) => {
+    // Validate the request body for required data
+    if (!req.body.item_name || !req.body.description || !req.body.category || !req.body.status || !req.body.quantity) {
+        return res.status(400).send('Please make sure to provide name, description, category, status and quantity fields in the request');
+    }
+  
+    try {
+        const data = await knex('inventories').insert(req.body);
+        const newInventoryURL = `/inventory/${data}`;
+        res.status(201).location(newInventoryURL).send(newInventoryURL);
+    } catch (err) {
+        res.status(400).send(`Error creating Inventory: ${err}`);
+    }
+  };
--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
 const inventoryController = require('../controllers/inventoryController');
 
+router.route('/').post(inventoryController.addInventory);
+
 module.exports = router;
