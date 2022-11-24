e68fec6f459e7512135b708e1721578c93f9bc24
codingisfun <61808280+jennyq7@users.noreply.github.com>
2022-11-24 11:57:08 -0500

    add routing to delete

--- controllers/warehouseController.js
+++ controllers/warehouseController.js
@@ -12,5 +12,9 @@ exports.index = async (req, res) => {
 };
 
 exports.deleteWarehouse = async (req, res) => {
-    
-}
+    try {
+        await knex('warehouses')
+        .where({id: req.params.id}).delete();
+        res.status(204).send(`Warehouse with id: ${req.params.id} has been deleted`);
+    } catch (err) {res.status(400).send(`Error deleting Warehouse ${req.params.id} ${err}`)};
+};
