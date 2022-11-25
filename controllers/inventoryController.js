efc29f0025a2c56fc22a0fab3a411ab149a135e9
Sarai Escobar <saraiee17@yahoo.com>
2022-11-25 17:54:27 -0600

    update data

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -5,13 +5,15 @@ const knex = require('knex')(require('../knexfile'));
 //API TO POST INVENTORY ITEM
 exports.addInventory = async (req, res) => {
     // Validate the request body for required data
-    if (!req.body.item_name || !req.body.description || !req.body.category || !req.body.status || !req.body.quantity) {
-        return res.status(400).send('Please make sure to provide name, description, category, status and quantity fields in the request');
+    if (!req.body.item_name || !req.body.description || !req.body.category || !req.body.status || !req.body.quantity|| !req.body.warehouse) {
+        return res.status(400).send('Please make sure to provide name, description, category, status, warehouse, and quantity fields in the request');
     }
   
     try {
         const data = await knex('inventories').insert(req.body);
-        const newInventoryURL = `/inventory/${data}`;
+        console.log(data)
+        const newInventoryURL = `/inventory/${data[0]}`;
+        console.log(newInventoryURL)
         res.status(201).location(newInventoryURL).send(newInventoryURL);
     } catch (err) {
         res.status(400).send(`Error creating Inventory: ${err}`);
