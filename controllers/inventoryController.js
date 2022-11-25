c7fbfdc2c3806020200eb9023a9b41727cabda11
Sarai Escobar <saraiee17@yahoo.com>
2022-11-25 11:30:58 -0600

    CORRECT LOGIC :)

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -1,19 +1,28 @@
 //Import knex so the controller has access to the DB
 const knex = require('knex')(require('../knexfile'));
 
-
-//API TO GET SINGLE INVENTORY ITEM FROM SINGLE WAREHOUSE
+//API TO GET SINGLE INVENTORY ITEM -KO26
 exports.singleInventory = async (req, res) => {
     try {
-        const data = await knex('inventories').where({id:req.params.id});
+        const data = await knex('inventories').join('warehouses','warehouses.id', 'inventories.warehouse_id').where({'inventories.id':req.params.id})
+        .select( "inventories.id",
+        "inventories.item_name",
+        "inventories.description",
+        "inventories.category",
+        "inventories.quantity", 
+        "inventories.status",
+        "warehouse_id",
+        "warehouses.warehouse_name"
+        )
         if (!data.length) {
             return res.status(404).send(`Record with id: ${req.params.id} is not found`);
         }
     
-        res.status(200).json(data);
+        res.status(200).json(data[0]);
     } catch (err) {
+        console.log(err)
         res.status(400).send(`Error retrieving inventory ${req.params.id} ${err}`)
     }
 };
 
-// line 9 --- const filter= await data.filter(item=>item.warehouse_id===req.params.id)
+
