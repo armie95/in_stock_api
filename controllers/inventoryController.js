97b95bd8c49bb4d1ee88a1f3361da98e33304ccd
armie95 <115040104+armie95@users.noreply.github.com>
2022-11-25 12:08:14 -0500

    Added the warehouse_name to the inventory items when all the inventory items are requrested. I kept the warehouse_id just incase we need it later

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -3,17 +3,20 @@ const knex = require("knex")(require("../knexfile"));
 
 exports.index = async (req, res) => {
   try {
-    const inventoriesData = await knex("inventories").select(
-      "id",
-      "warehouse_id",
-      "item_name",
-      "description",
-      "category",
-      "status",
-      "quantity",
-      "created_at",
-      "updated_at"
-    );
+    const inventoriesData = await knex("inventories")
+      .join("warehouses", "warehouses.id", "=", "inventories.warehouse_id")
+      .select(
+        "inventories.id",
+        "inventories.warehouse_id",
+        "warehouses.warehouse_name",
+        "inventories.item_name",
+        "inventories.description",
+        "inventories.category",
+        "inventories.status",
+        "inventories.quantity",
+        "inventories.created_at",
+        "inventories.updated_at"
+      );
     res.status(200).json(inventoriesData);
   } catch (err) {
     res.status(400).send(`Error retrieving Warehouses: ${err}`);
