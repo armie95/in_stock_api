e995930bb4158509657c95a47f20e4d7d11a3238
Sarai Escobar <saraiee17@yahoo.com>
2022-11-25 19:40:58 -0600

    uuid

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -1,19 +1,21 @@
 //Import knex so the controller has access to the DB
 const knex = require('knex')(require('../knexfile')); 
+const {v4: uuidv4} = require('uuid');
 
 
 //API TO POST INVENTORY ITEM
-exports.addInventory = async (req, res) => {
+exports.addInventory= async (req, res) => {
     // Validate the request body for required data
-    if (!req.body.item_name || !req.body.description || !req.body.category || !req.body.status || !req.body.quantity|| !req.body.warehouse) {
+    console.log(req.body)
+    if (!req.body.item_name || !req.body.description || !req.body.category || !req.body.status || !req.body.quantity|| !req.body.warehouse_name){
         return res.status(400).send('Please make sure to provide name, description, category, status, warehouse, and quantity fields in the request');
     }
   
     try {
-        const data = await knex('inventories').insert(req.body);
-        console.log(data)
-        const newInventoryURL = `/inventory/${data[0]}`;
-        console.log(newInventoryURL)
+        const newInventory = req.body;
+        newInventory.id = uuidv4();
+        const data = await knex('inventories').insert(newInventory);
+        const newInventoryURL = `/inventory/${newInventory.id}`;
         res.status(201).location(newInventoryURL).send(newInventoryURL);
     } catch (err) {
         res.status(400).send(`Error creating Inventory: ${err}`);
