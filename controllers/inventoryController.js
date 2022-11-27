f6f69cbcfd9daea8a4b651e3d18e2c9d7631ce11
Sarai Escobar <saraiee17@yahoo.com>
2022-11-27 14:44:48 -0600

    debug

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -3,11 +3,11 @@ const knex = require('knex')(require('../knexfile'));
 const {v4: uuidv4} = require('uuid');
 
 
-//API TO POST INVENTORY ITEM
+//API TO POST INVENTORY ITEMnode 
 exports.addInventory= async (req, res) => {
     // Validate the request body for required data
     console.log(req.body)
-    if (!req.body.item_name || !req.body.description || !req.body.category || !req.body.status || !req.body.quantity|| !req.body.warehouse_name){
+    if (!req.body.item_name||!req.body.description||!req.body.category||!req.body.quantity||!req.body.status ||!req.body.warehouse_id){
         return res.status(400).send('Please make sure to provide name, description, category, status, warehouse, and quantity fields in the request');
     }
   
