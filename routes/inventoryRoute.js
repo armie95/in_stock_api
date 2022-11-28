651ac3fca5dc17d4989ef33182bd7a9ac45342e3
NickSigurdsson <nicholassigurdsson@gmail.com>
2022-11-28 11:57:59 -0500

    edit inventory await conversion

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
 const inventoryController = require('../controllers/inventoryController');
+const inventoryEditController = require('../controllers/inventoryEditController');
+
 
 //API TO GET SINGLE INVENTORY ITEM FROM SINGLE WAREHOUSE
-router
-  .route('/:id')
-  .get(inventoryController.singleInventory)
+router.route('/:id').get(inventoryController.singleInventory)
+router.route('/:id/edit').put(inventoryEditController.updateInventory);
 
 
 module.exports = router;
