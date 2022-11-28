2628b771724335e75dc7da096890a5aeabae2fb6
codingisfun <61808280+jennyq7@users.noreply.github.com>
2022-11-28 18:57:36 -0500

    fix formatting

--- controllers/warehouseController.js
+++ controllers/warehouseController.js
@@ -2,56 +2,54 @@
 const knex = require("knex")(require("../knexfile"));
 
 exports.index = async (req, res) => {
-    try {
-        const warehouseData = await knex("warehouses").select(
-          "id",
-          "warehouse_name",
-          "address",
-          "city",
-          "country",
-          "contact_name",
-          "contact_phone",
-          "contact_email"
-        );
-        res.status(200).json(warehouseData);
-      } catch (err) {
-        res.status(400).send(`Error retrieving Warehouses: ${err}`);
-      };
+  try {
+    const warehouseData = await knex("warehouses").select(
+      "id",
+      "warehouse_name",
+      "address",
+      "city",
+      "country",
+      "contact_name",
+      "contact_phone",
+      "contact_email"
+    );
+    res.status(200).json(warehouseData);
+  } catch (err) {
+    res.status(400).send(`Error retrieving Warehouses: ${err}`);
+  };
 };
 
+//function to get warehouse info on pageload
 exports.getInfoForEdit = async (req, res) => {
-    try { 
-        const warehouseData = await knex('warehouses')
-        .select('id', 'warehouse_name', 'address', 'city', 'country', 'contact_name', 'contact_position', 'contact_phone', 'contact_email')
-        .where({id: req.params.id})
-        res.status(200).json(warehouseData);
-    } catch (err) {
-        res.status(400).send(`Error retrieving Warehouses: ${err}`)
-    }
+  try {
+    const warehouseData = await knex('warehouses')
+      .select('id', 'warehouse_name', 'address', 'city', 'country', 'contact_name', 'contact_position', 'contact_phone', 'contact_email')
+      .where({ id: req.params.id })
+    res.status(200).json(warehouseData);
+  } catch (err) {
+    res.status(400).send(`Error retrieving Warehouses: ${err}`)
+  }
 };
 
-
+//function to edit the info
 exports.editWarehouse = async (req, res) => {
-   
-        const re = /^(\+|00)[1-9][0-9 \-\(\)\.]{7,32}$/;
-        const emailPattern = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-        if(!req.body.warehouse_name || !req.body.address || !req.body.city || !req.body.country || !req.body.contact_name || !req.body.contact_position || !req.body.contact_phone || !req.body.contact_email)
-        {return res.status(400).send('Please complete all fields')}
-        else if (!((req.body.contact_phone).match(re))) {
-            return res.status(400).send('Please follow phone pattern')
-        }
-        else if(!((req.body.contact_email).match(emailPattern))){
-            return res.status(400).send('Please follow email pattern')
-        }
-try {
+  const re = /^(\+|00)[1-9][0-9 \-\(\)\.]{7,32}$/;
+  const emailPattern = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  if (!req.body.warehouse_name || !req.body.address || !req.body.city || !req.body.country || !req.body.contact_name || !req.body.contact_position || !req.body.contact_phone || !req.body.contact_email) { return res.status(400).send('Please complete all fields') }
+  else if (!((req.body.contact_phone).match(re))) {
+    return res.status(400).send('Please follow phone pattern')
+  }
+  else if (!((req.body.contact_email).match(emailPattern))) {
+    return res.status(400).send('Please follow email pattern')
+  }
+  try {
     await knex('warehouses')
-    .where({id: req.params.id}).update(req.body);
+      .where({ id: req.params.id }).update(req.body);
     res.status(200).send(`Warehouse with id: ${req.params.id} has been updated`);
-    } 
-    catch (err) {res.status(400).send(`Error updating Warehouse ${req.params.id} ${err}`)};
-    
-};
+  }
+  catch (err) { res.status(400).send(`Error updating Warehouse ${req.params.id} ${err}`) };
 
+};
 
 
 // Get all warehouse data by warehouse id:
