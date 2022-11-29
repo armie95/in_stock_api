212d5495a784b5fba8cabc37e8aa5ddaee5bfd6a
NickSigurdsson <nicholassigurdsson@gmail.com>
2022-11-28 22:40:36 -0500

    changed route paths for controller

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -1,7 +1,5 @@
 const router = require('express').Router();
 const inventoryController = require('../controllers/inventoryController');
-const inventoryEditController = require('../controllers/inventoryEditController');
-
 
 router.route('/').get(inventoryController.index);
 router.route('/').post(inventoryController.addInventory);
@@ -9,7 +7,7 @@ router.route('/').post(inventoryController.addInventory);
 //API TO GET SINGLE INVENTORY ITEM FROM SINGLE WAREHOUSE
 
 router.route('/:id').get(inventoryController.singleInventory)
-router.route('/:id/edit').put(inventoryEditController.updateInventory);
+router.route('/:id/edit').put(inventoryController.updateInventory);
 //API TO DELETE A SINGLE INVENTORY ITEM FROM SINGLE WAREHOUSE
 router.route('/:id').delete(inventoryController.deleteInventory);
 
